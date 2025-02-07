031eba951cd2ac52a80836de97b157db604d4089
m-aksoy-altug <m.aksoy.altug@gmail.com>
2025-02-07 15:07:25 +1100

    Spring core beans

--- src/main/java/com/spring/core/SpringApp.java
+++ src/main/java/com/spring/core/SpringApp.java
@@ -1,6 +1,11 @@
 package com.spring.core;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.spring.core.service.AppService;
@@ -8,11 +13,20 @@ import com.spring.core.service.AppService;
 public class SpringApp {
 	
 	public static void main(String[] args) {
+				
         ApplicationContext context = 
         		new ClassPathXmlApplicationContext("applicationContext.xml");
         AppService appService = (AppService) context.getBean("appService");
         appService.test();
-        ((ClassPathXmlApplicationContext) context).close();	
+        ((ClassPathXmlApplicationContext) context).close();
+        
+
+        ApplicationContext contextD = new AnnotationConfigApplicationContext();
+        String[] beanNames = contextD.getBeanDefinitionNames();
+        List<String> coreBeans = Arrays.stream(beanNames).collect(Collectors.toList());
+        System.out.println("Spring Core Beans Registered by Default:");
+        coreBeans.forEach(System.out::println);
+        ((AnnotationConfigApplicationContext) contextD).close();
   }
 		
 }
